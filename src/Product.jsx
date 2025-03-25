ad47f58d298e3302ff2eeeb964449719b4b2bd26
sravanthi0603 <sravanthivanam06@gmail.com>
2025-03-25 14:45:11 +0530

    Add files via upload

---
+++ src/Product.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './style.css'
+
+function Product() {
+  return (
+    <>
+    <h1 className='title'>Product</h1>
+    </>
+  )
+}
+
+export default Product
